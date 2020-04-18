f1274546c1773fb26c7b1a70dad43307c07bfeac
Nguyen <nguyen.nguyen@tala.co>
2020-04-17 20:57:41 -0700

    added docker for flask.

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, url_for, render_template, request
-from markupsafe import escape
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-	return render_template('index.html')
