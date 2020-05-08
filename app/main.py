a064b3f30516292b69ffff24bd923dd4976444b0
martyboggs <whack47@gmail.com>
2020-05-08 16:05:35 -0700

    test commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 
 import sys
 import os
+from flask import render_template
 
 # adding this part so we always do absolute import
 myPath = os.path.dirname(os.path.abspath(__file__))
@@ -9,18 +10,17 @@ sys.path.insert(0, myPath + '/../')
 from app.routes import app
 
 
-# TODO: index.html not found?
-# @app.route('/')
-# def hello_world():
-# 	return render_template('index.html')
+@app.route('/')
+def hello_world():
+	return render_template('index.html')
 
-@app.route("/")
-def hello():
-  version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
-  message = "Hello World from Flask in a uWSGI Nginx Docker container with Python {} (default)".format(
-    version
-  )
-  return message
+# @app.route("/")
+# def hello():
+#   version = "{}.{}".format(sys.version_info.major, sys.version_info.minor)
+#   message = "Hello World from Flask in a uWSGI Nginx Docker container with Python {} (default)".format(
+#     version
+#   )
+#   return message
 
 
 if __name__ == "__main__":
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 import pkgutil
 import re
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates")
 
 
 # from main import *
