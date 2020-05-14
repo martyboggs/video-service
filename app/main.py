b9a1f7ac94aee644451e14d5bff58e666aa459a5
martyboggs <whack47@gmail.com>
2020-05-14 11:30:54 -0700

    get videos from s3

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
 
 import sys
 import os
+import boto3
 from flask import Flask, render_template
 from routes.video_routes import video_api
 from routes.test_routes import test_api
+# pylint: disable=E1101
 
 app = Flask(__name__)
 
 app.register_blueprint(video_api, url_prefix='/video')
 app.register_blueprint(test_api, url_prefix='/test')
 
-
 @app.route('/')
+def index():
+  s3 = boto3.resource('s3')
+  # for bucket in s3.buckets.all():
+  #   print(bucket.name)
+  bucketName = 'video-service2'
+  bucket = s3.Bucket(bucketName)
+  files = bucket.objects.all()
+  return render_template('index.html', data=files)
+
+@app.route('/upload')
 def hello_world():
-	return render_template('index.html')
+  return render_template('upload.html')
 
 @app.route("/hello")
 def hello():
@@ -22,7 +33,6 @@ def hello():
     version
   )
   return message
-
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True, port=80)
